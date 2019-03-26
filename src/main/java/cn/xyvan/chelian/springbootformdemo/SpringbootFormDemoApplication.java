6cbb707114e509acc4dcc1dcfb22157ef4123213
8574526@qq.com <q8574526>
2019-03-26 23:06:24 +0800

    First commit

---
+++ src/main/java/cn/xyvan/chelian/springbootformdemo/SpringbootFormDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.xyvan.chelian.springbootformdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFormDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFormDemoApplication.class, args);
+    }
+
+}
